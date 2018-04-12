7b89e9865e45b32fa51e488239019d304b67fc96
mike.singleton <mike.singleton@verafin.com>
2018-04-12 20:39:37 -0230

    deck

---
+++ src/main/kotlin/com/pokerplayer/util/Deck.kt
@@ -0,0 +1,30 @@
+package com.pokerplayer.util
+
+import com.pokerplayer.model.Card
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Deck() {
+    private var cards: ArrayDeque<Card> = ArrayDeque()
+
+    val size: Int
+        get() = cards.size
+
+    init {
+        val list = ArrayList(Card.allCards)
+        list.shuffle()
+        cards.addAll(list)
+    }
+
+    fun draw(): Card {
+        return cards.pop()
+    }
+
+    fun shuffle() {
+        val tempList = ArrayList(cards.toList())
+        tempList.shuffle()
+        val newDeck = ArrayDeque<Card>()
+        newDeck.addAll(tempList)
+        cards = newDeck
+    }
+}
