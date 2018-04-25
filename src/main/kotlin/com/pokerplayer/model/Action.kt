182b845cb9ddc232528fa3efb0a9e340d66dda24
= <=>
2018-04-25 20:31:13 -0300

    Cleanup of Card Companion and Action code for readability.

--- src/main/kotlin/com/pokerplayer/model/Action.kt
+++ src/main/kotlin/com/pokerplayer/model/Action.kt
@@ -1,7 +1,8 @@
 package com.pokerplayer.model
 
-sealed class Action
-class Fold : Action()
-class Check : Action()
-data class Call(val amount: Int) : Action()
-data class Raise(val amount: Int) : Action()
+sealed class Action {
+    class Fold : Action()
+    class Check : Action()
+    data class Call(val amount: Int) : Action()
+    data class Raise(val amount: Int) : Action()
+}
