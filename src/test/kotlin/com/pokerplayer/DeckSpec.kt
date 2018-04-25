5b27176d7c09f11a0b37ec413ca8c7863163f968
mike.singleton <mike.singleton@verafin.com>
2018-04-25 13:44:07 -0230

    Made Speks individually runn able. Added Junit Test tests (lol)

--- src/test/kotlin/com/pokerplayer/DeckSpec.kt
+++ src/test/kotlin/com/pokerplayer/DeckSpec.kt
@@ -7,7 +7,10 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
 
+@RunWith(JUnitPlatform::class)
 object DeckSpec : Spek({
     given("A Deck of cards") {
         val deck = Deck()
