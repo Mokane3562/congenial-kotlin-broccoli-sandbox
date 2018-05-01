405de1430c267adde882ddf3b43b83aa35e58098
Scott <Scott-Young-6746@users.noreply.github.com>
2018-05-01 15:20:12 -0300

    Update Action.kt

--- src/main/kotlin/com/pokerplayer/model/Action.kt
+++ src/main/kotlin/com/pokerplayer/model/Action.kt
@@ -1,8 +1,7 @@
 package com.pokerplayer.model
 
-sealed class Action {
-    class Fold : Action()
-    class Check : Action()
-    data class Call(val amount: Int) : Action()
-    data class Raise(val amount: Int) : Action()
-}
+sealed class Action
+class Fold : Action()
+class Check : Action()
+data class Call(val amount: Int) : Action()
+data class Raise(val amount: Int) : Action()
